4dadc5ace2736b960bab92caa7b55f195f6a5c9b
Cristian Murillo <cam_725@hotmail.com>
2017-04-19 00:01:48 -0500

    Added delete from MongoBD database file

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,26 @@
+
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if(err){
+        return console.log('Unable to connect to MongoDB server');
+    }
+    console.log('Connected to MongoDB server');
+
+    //deleteMany
+    db.collection('Users').deleteMany({name: 'Cristian Murillo'}).then((result) => {
+        console.log(result);
+    });
+
+    //deleteOne
+    db.collection('Todos').deleteOne({ text: 'Eat lunch'}).then((result) => {
+        console.log(result);
+    });
+
+    //findOneAndDelete
+
+    db.collection('Users').findOneAndDelete({_id: new ObjectID('58f6d4eb1c891c159009dfce')}).then((result) => {
+        console.log(result);
+    });
+    //db.close();
+});
