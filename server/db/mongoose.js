9f8bd09c09e48236855084b7f2e18fdbd40402a2
Cristian Murillo <cam_725@hotmail.com>
2017-04-22 16:26:44 -0500

    Added connection to mongoLab database

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+//mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://cristian.murillo87:capi1cmu@ds115701.mlab.com:15701/todoapp');
 
 module.exports = {mongoose};
