f161f72c066eb8d35c5e3eeb62013af523395fc4
Cristian Murillo <cam_725@hotmail.com>
2017-04-22 16:50:50 -0500

    Fixed mongodb url

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-//mongoose.connect('mongodb://localhost:27017/TodoApp');
-mongoose.connect('mongodb://cristian.murillo87:capi1cmu@ds115701.mlab.com:15701/todoapp');
+mongoose.connect('mongodb://localhost:27017/TodoApp');
+
 
 module.exports = {mongoose};
